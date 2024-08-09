1bfe35bcf680c520dd341bd14d50eca7a968322e
unknown <vinayak20injamure@gmail.com>
2024-08-09 23:05:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CardComponent } from './card/card.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angfirst';
+  isDsabled:boolean=true;
+  isActive:boolean=false;  
 }
