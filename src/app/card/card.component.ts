f673557c9f0c73321b752e7c3681aa3b6382706b
unknown <vinayak20injamure@gmail.com>
2024-08-10 21:46:53 +0530

    m

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
